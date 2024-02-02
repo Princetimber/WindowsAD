d98cfe4c97e5ea860bfd9aca6fed734c7fec43f9
Olamide Olaleye <Olamide.Olaleye@outlook.com>
2024-02-02 11:39:03 +0000

    added support for Should Process.

--- ActiveDirectory/groups/New-Group.psm1
+++ ActiveDirectory/groups/New-Group.psm1
@@ -1,50 +1,56 @@
-function New-Group {
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory = $true)][ValidateScript({
-        if (-not(Get-ADGroup -Filter { Name -Like "$($_)" } -ErrorAction SilentlyContinue)) {
-          return $true
-        }
-        else {
-          throw "Group '$_' already exists."
-        }
-      })]
-    [string]$Name,
-    [Parameter(Mandatory = $true)]
-    [string]$Path,
-    [Parameter(Mandatory = $true)][ValidateSet('Security', 'Distribution')]
-    [string]$GroupCategory,
-    [Parameter(Mandatory = $true)][ValidateSet('Global', 'DomainLocal', 'Universal')]
-    [string]$GroupScope,
-    [Parameter(Mandatory = $false)]
-    [string]$DisplayName,
-    [Parameter(Mandatory = $false)]
-    [string]$Description
-  )
-  ## Validate the Path parameter.
-  if ($Path -notmatch '^(?:OU=[^,]+,?)*(?:DC=[^,]+,?)*$') {
-    throw "The Path parameter must be in the format OU=?,DC=?,DC?"
-    return
-  }
-  ## Create a hash table of parameters for the New-ADGroup cmdlet.
-  $createParams = @{}
-  $createParams['Name'] = $Name
-  $createParams['Path'] = $Path
-  $createParams['GroupCategory'] = $GroupCategory
-  $createParams['GroupScope'] = $GroupScope
-  ## The following parameters are optional.
-  if ($Description) {
-    $createParams['Description'] = $Description
-  }
-  ## Create the group.
-  try {
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+  [Parameter(Mandatory = $true)][ValidateScript({
+      if (-not(Get-ADGroup -Filter { Name -Like "$($_)" } -ErrorAction SilentlyContinue)) {
+        return $true
+      }
+      else {
+        throw "Group '$_' already exists."
+      }
+    })]
+  [string]$Name,
+  [Parameter(Mandatory = $true)]
+  [string]$Path,
+  [Parameter(Mandatory = $true)][ValidateSet('Security', 'Distribution')]
+  [string]$GroupCategory,
+  [Parameter(Mandatory = $true)][ValidateSet('Global', 'DomainLocal', 'Universal')]
+  [string]$GroupScope,
+  [Parameter(Mandatory = $false)]
+  [string]$DisplayName,
+  [Parameter(Mandatory = $false)]
+  [string]$SamAccountName,
+  [Parameter(Mandatory = $false)]
+  [string]$Description
+)
+## Validate the Path parameter.
+if ($Path -notmatch '^(?:OU=[^,]+,?)*(?:DC=[^,]+,?)*$') {
+  throw "The Path parameter must be in the format OU=?,DC=?,DC?"
+  return
+}
+## Create a hash table of parameters for the New-ADGroup cmdlet.
+$createParams = @{}
+$createParams['Name'] = $Name
+$createParams['Path'] = $Path
+$createParams['GroupCategory'] = $GroupCategory
+$createParams['GroupScope'] = $GroupScope
+## The following parameters are optional.
+if ($Description) {
+  $createParams['Description'] = $Description
+}
+## Create the group.
+try {
+  if ($PSCmdlet.ShouldProcess($Name, 'Create a new group')) {
     New-ADGroup @createParams
   }
-  catch {
-    $errorMessage = $_.Exception.Message
-    throw "Failed to create group '$Name' in '$Path'. Error: $errorMessage"
+  elseif ($PSCmdlet.ShouldContinue("Do you want to continue?")) {
+    New-ADGroup @createParams
   }
 }
+catch {
+  $errorMessage = $_.Exception.Message
+  throw "Failed to create group '$Name' in '$Path'. Error: $errorMessage"
+}
+
 function Update-ADPrincipalGroupMemebership {
   [CmdletBinding(DefaultParameterSetName = 'AddPrincipalGroupMembership, RemovePrincipalGroupMembership')]
   param(
