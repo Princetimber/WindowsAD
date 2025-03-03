9449b6e1ece26f678059241e67121aebcc88f1e7
Olamide Olaleye <olamide.olaleye@outlook.com>
2025-03-03 15:39:43 +0000

    Updated configurations - included  values

--- security/check USB Storage Status.ps1
+++ security/check USB Storage Status.ps1
@@ -6,26 +6,32 @@
 
 # Check USB Storage Status
 function Confirm-USBStorageStatus {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $false)]
-        [string]$Path = "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR"
-    )
-    try {
-        $usbStorageEnabled = (Get-ItemProperty -Path $Path -Name "Start").Start
-        # Ensure exact compliance output
-        if ($usbStorageEnabled -eq 4) {
-            Write-Output "Compliant"
-            exit 0
-        }
-        else {
-            Write-Output "Non-Compliant"
-            exit 1
-        }
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $false)]
+    [string]$Path = "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR"
+  )
+  try {
+    $usbStorageEnabled = (Get-ItemProperty -Path $Path -Name "Start").Start
+    # Ensure exact compliance output
+    if ($usbStorageEnabled -eq 4) {
+      Write-Output "Compliant"
+      $hash = @{USBStorageCompliance = "Compliant"; USBStorageStatus = "Disabled" }
+      return $hash | ConvertTo-Json -Compress
+      exit 0
     }
-    catch {
-        Write-Output "Non-Compliant"
-        exit 1
+    else {
+      Write-Output "Non-Compliant"
+      $hash = @{USBStorageCompliance = "Non-Compliant"; USBStorageStatus = "Enabled" }
+      return $hash | ConvertTo-Json -Compress
+      exit 1
     }
+  }
+  catch {
+    Write-Output "Non-Compliant"
+    $hash = @{USBStorageCompliance = "Non-Compliant"; USBStorageStatus = "Enabled" }
+    return $hash | ConvertTo-Json -Compress values
+    exit 1
+  }
 }
 
